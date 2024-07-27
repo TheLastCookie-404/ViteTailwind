f03c06635bd5b0494ebd02e4501626306a5cb2d0
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2024-07-27 12:15:04 +0500

    tech commit

--- vite-tailwind/vite.config.js
+++ vite-tailwind/vite.config.js
@@ -1,14 +1,19 @@
-import { resolve } from 'path'
+// import { resolve } from 'path'
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  build: {
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        home: resolve(__dirname, 'src/pages/home.html'),
-        main: resolve(__dirname, 'src/assets/main.css'),
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       index: resolve(__dirname, 'index.html'),
+  //       home: resolve(__dirname, 'src/pages/home.html'),
+  //       main: resolve(__dirname, 'src/assets/main.css'),
+  //     },
+  //     output: {
+  //       index: resolve(__dirname, 'index.html'),
+  //       home: resolve(__dirname, 'home.html'),
+  //       main: resolve(__dirname, 'main.css'),
+  //     }
+  //   },
+  // },
 })
